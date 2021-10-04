281bc50e93772f2e94e34e880cd63c599bd84d8f
Josephina <jyergatianmurfitt@googlemail.com>
2021-10-04 15:35:43 +0100

    fist commit

---
+++ main.js
@@ -0,0 +1,259 @@
+const productDataWomens = [{
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/dark-grey-faux-fur-longline-coat/p/619712403',
+  imageSrc: 'https://media2.newlookassets.com/i/newlook/619712403.jpg',
+  productTitle: 'Dark Grey Faux Fur Longline Coat',
+  price: '45.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/tops/black-ribbed-long-sleeve-roll-neck-top/p/635105501',
+  imageSrc: 'https://media2.newlookassets.com/i/newlook/635105501.jpg',
+  productTitle: 'Black Ribbed Long Sleeve Roll Neck Top',
+  price: '8.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/leggings/black-coated-leather-look-leggings/p/634018201',
+  imageSrc: 'https://media3.newlookassets.com/i/newlook/634018201.jpg',
+  productTitle: 'Black Coated Leather-Look Leggings',
+  price: '19.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/black-faux-fur-longline-coat/p/619712401',
+  imageSrc: 'https://media3.newlookassets.com/i/newlook/619712401.jpg',
+  productTitle: 'Black Faux Fur Longline Coat',
+  price: '45.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-high-waist-super-skinny-hallie-jeans/p/635654901',
+  imageSrc: 'https://media2.newlookassets.com/i/newlook/635654901.jpg',
+  productTitle: 'Black High Waist Super Skinny Hallie Jeans',
+  price: '25.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/camel-revere-collar-coat/p/619653214',
+  imageSrc: 'https://media2.newlookassets.com/i/newlook/619653214.jpg',
+  productTitle: 'Camel Revere Collar Coat',
+  price: '35.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-%27lift-%26-shape%27-jeggings/p/623140201',
+  imageSrc: 'https://media3.newlookassets.com/i/newlook/623140201.jpg',
+  productTitle: 'Black Lift & Shape Jeggings',
+  price: '17.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/black-leather-look-aviator-jacket/p/632207701',
+  imageSrc: 'https://media2.newlookassets.com/i/newlook/632207701.jpg',
+  productTitle: 'Black Leather-Look Aviator Jacket',
+  price: '55.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/light-grey-fluffy-houndstooth-double-breasted-coat-/p/631197208',
+  imageSrc: 'https://media3.newlookassets.com/i/newlook/631197208.jpg',
+  productTitle: 'Light Grey Fluffy Houndstooth Double Breasted Coat',
+  price: '49.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/leggings/black-high-waist-leggings/p/616323901',
+  imageSrc: 'https://media3.newlookassets.com/i/newlook/616323901.jpg',
+  productTitle: 'Black High Waist Leggings',
+  price: '8.99'
+},
+{
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-coated-super-skinny-hallie-jeans/p/631910801',
+  imageSrc: 'https://media2.newlookassets.com/i/newlook/631910801.jpg',
+  productTitle: 'Black Coated Super Skinny Hallie Jeans',
+  price: '27.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/footwear/boots/black-leather-look-chunky-lace-up-boots/p/629029201',
+  imageSrc: 'https://media3.newlookassets.com/i/newlook/629029201.jpg',
+  productTitle: 'Black Leather-Look Chunky Lace Up Boots',
+  price: '27.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-%27lift-%26-shape%27-high-waist-skinny-jeans/p/631911801',
+  imageSrc: 'https://media3.newlookassets.com/i/newlook/631911801.jpg',
+  productTitle: 'Black Lift & Shape High Waist Skinny Jeans',
+  price: '25.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/footwear/boots/wide-fit-black-teddy-lined-chunky-lace-up-boots/p/637716701',
+  imageSrc: 'https://media3.newlookassets.com/i/newlook/637716701.jpg',
+  productTitle: 'Wide Fit Black Teddy Lined Chunky Lace Up Boots',
+  price: '27.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/dark-green-faux-fur-longline-coat/p/619712438',
+  imageSrc: 'https://media3.newlookassets.com/i/newlook/619712438.jpg',
+  productTitle: 'Dark Green Faux Fur Longline Coat',
+  price: '45.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/nightwear/pale-grey-fluffy-hooded-dressing-gown/p/629877202',
+  imageSrc: 'https://media3.newlookassets.com/i/newlook/629877202.jpg',
+  productTitle: 'Pale Grey Fluffy Hooded Dressing Gown',
+  price: '25.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/pale-grey-revere-collar-coat/p/619653202',
+  imageSrc: 'https://media2.newlookassets.com/i/newlook/619653202.jpg',
+  productTitle: 'Pale Grey Revere Collar Coat',
+  price: '35.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/jeans/black-super-soft-super-skinny-india-jeans/p/623185401',
+  imageSrc: 'https://media2.newlookassets.com/i/newlook/623185401.jpg',
+  productTitle: 'Black Super Soft Super Skinny India Jeans',
+  price: '22.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/clothing/coats-jackets/black-leather-look-biker-jacket-/p/620187701',
+  imageSrc: 'https://media2.newlookassets.com/i/newlook/620187701.jpg',
+  productTitle: 'Black Leather-Look Biker Jacket',
+  price: '32.99'
+}, {
+  productUrl: 'https://www.newlook.com/uk/womens/footwear/boots/black-leather-flat-chelsea-boots/p/633518801',
+  imageSrc: 'https://media2.newlookassets.com/i/newlook/633518801.jpg',
+  productTitle: 'Black Leather Flat Chelsea Boots',
+  price: '35.99'
+}]
+
+const render = () => {
+  const products = document.querySelector('.products');
+  const filtersBtn = document.querySelector('.filtersBtn');
+  const filters = document.querySelector('.filters');
+  const openFilterBtns = document.querySelectorAll('.openFilter');
+  const chooseBtn = document.querySelector('.chooseBtn');
+  const options = document.querySelectorAll('.option');
+  const categoryBtn = document.querySelector('.categoryBtn');
+  const searchInput = document.querySelector('.searchInput');
+  const minPrice = document.querySelector('.minPrice');
+  const maxPrice = document.querySelector('.maxPrice');
+  const validatePrice = new RegExp('^[0-9]*$');
+  const popover = document.querySelector('.popover');
+  const errorMsg = document.querySelector('.errorMsg');
+  const clearBtn = document.querySelector('.clearBtn');
+
+  const overlay = document.querySelector('.overlay');
+  const exit = document.querySelector('.exit');
+  const overlayImg = document.querySelector('.overlayImg');
+  const linkBtn = document.querySelector('.linkBtn');
+
+  let displayedProducts = productDataWomens;
+
+  /////////////////////////////Render products///////////////////////////////
+  const construct = () => {
+    products.innerHTML = '';
+    if (displayedProducts.length == 0) {
+      errorMsg.style.display = 'block';
+    } else {
+      errorMsg.style.display = 'none';
+      displayedProducts.forEach((product, i) => {
+        let productContainer = document.createElement('div');
+        productContainer.className = 'productContainer';
+        productContainer.alt = product.productTitle;
+
+        let productName = document.createElement('h5');
+        productName.textContent = product.productTitle;
+        productName.classList = 'productName text'
+
+        let productImg = document.createElement('img');
+        productImg.src = product.imageSrc;
+        productImg.className = 'productImg';
+
+        let productPrice = document.createElement('h6');
+        productPrice.textContent = "£" + product.price;
+        productPrice.value = product.price;
+        productPrice.classList = 'productPrice text';
+
+        let productLink = document.createElement('a')
+        productLink.href = product.productUrl;
+
+        products.appendChild(productContainer);
+        productContainer.appendChild(productImg);
+        productContainer.appendChild(productName);
+        productContainer.appendChild(productPrice);
+        productContainer.appendChild(productLink);
+
+        productContainer.addEventListener('click', i = () => {
+          overlay.style.display = 'block';
+          overlayImg.src = product.imageSrc;
+          linkBtn.href = product.productUrl;
+          linkBtn.className = 'productLink';
+          linkBtn.innerText = 'View product';
+          linkBtn.target = '_blank';
+          linkBtn.rel = "noopener noreferrer";
+        });
+      });
+    }
+  }
+  construct();
+
+  //Pop-up exit//
+  exit.addEventListener('click', i = () => {
+    overlay.style.display = 'none';
+  })
+
+
+  /////////////////////////////Display filters///////////////////////////////
+  //Display filters and change button text//
+  filtersBtn.addEventListener('click', i = () => {
+    if(filters.classList == "filters open") {
+      filters.classList.remove('open');
+      filtersBtn.innerHTML = 'Filter';
+    } else {
+      filters.classList.add('open');
+      filtersBtn.innerHTML = 'Apply';
+    }
+  })
+  //Open each filter on click & call categories filter//
+  if(window.innerWidth < 720) {
+    openFilterBtns.forEach((button, i) => {
+      button.addEventListener('click', e = () => {
+        button.classList.toggle('open');
+        options.forEach((option, i) => {
+          option.addEventListener('click', e = () => {
+            button.classList.remove('open');
+            runCategories(option.innerText);
+          })
+        });
+      })
+    });
+  }
+  //Open categories list on desktop & call categories filter//
+
+
+
+  /////////////////////////////Run filters///////////////////////////////
+  //Clear filters//
+  const clearFilters = () => {
+    displayedProducts = [];
+  }
+
+
+  //Search filter//
+  const applySearch = product => {
+    const searchText = new RegExp(searchInput.value, 'i');
+    return searchText.test(product.productTitle);
+  }
+  searchInput.addEventListener('keyup', e = () => {
+    if(searchInput.value.length > 0) {
+      clearFilters();
+      let searchedProducts = productDataWomens.filter(applySearch);
+      searchedProducts.forEach((product, i) => {
+        displayedProducts.push(product);
+        console.log(displayedProducts)
+      });
+      construct();
+    }
+  })
+
+  //Price filter//
+
+
+  //Category filter//
+
+
+  ////////////////////////Slide carousel with arrows///////////////////////
+  const leftArrow = document.querySelector('.leftArrow');
+  const rightArrow = document.querySelector('.rightArrow');
+  const topArrow = document.querySelector('.topArrow');
+  const bottomArrow = document.querySelector('.bottomArrow');
+
+  rightArrow.addEventListener('click', f = () => {
+    products.scrollBy(window.innerWidth, 0);
+  })
+  leftArrow.addEventListener('click', e = () => {
+    products.scrollBy(-window.innerWidth, 0);
+  })
+  topArrow.addEventListener('click', f = () => {
+    products.scrollBy(0, -100);
+  })
+  bottomArrow.addEventListener('click', e = () => {
+    products.scrollBy(0, 200);
+  })
+
+}
